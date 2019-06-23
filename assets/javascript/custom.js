b72fdb38a532a6927967134b8368a410b1123dae
J Lobos Vega <jalv.vg@gmail.com>
2019-06-23 07:10:16 -0400

    Update custom.js

--- assets/javascript/custom.js
+++ assets/javascript/custom.js
@@ -9,3 +9,27 @@ $(document).ready(function() {
         }
     });
 });
+/*===========Close mobile nav on click==========*/
+$(document).ready(function() {
+    $(document).click(function(event) {
+        var clickover = $(event.target);
+        var _opened = $(".navbar-collapse").hasClass("show");
+        if ( _opened === true && !clickover.hasClass("navbar-toggler")) {
+            $(".navbar-toggler").click();
+        }
+    })
+})
+/*===========Smooooooth scroooling to links==========*/
+$(document).ready(function() {
+    $("a").on('click', function(event) {
+        if (this.hash !=="") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate( {
+                scrollTop: $(hash).offset().top
+            }, 800, function() {
+                window.location.hash = hash;
+            });
+        } //End  if
+    });
+});
